c7f089a77dd7d9d3205c8bd7d91b1b82d91288f4
xupenghao <xupenghao1996@163.com>
2021-08-26 07:25:42 +0800

    迭代器模式

---
+++ behavior/iterator/iterator.go
@@ -0,0 +1,47 @@
+package iterator
+
+type user struct {
+	name string
+	age  int
+}
+
+type iterator interface {
+	hasNext() bool
+	getNext() *user
+}
+
+type collection interface {
+	createIterator() iterator
+}
+
+type userCollection struct {
+	users []*user
+}
+
+func (u *userCollection) createIterator() iterator {
+	return &userIterator{
+		users: u.users,
+	}
+}
+
+type userIterator struct {
+	index int
+	users []*user
+}
+
+func (u *userIterator) hasNext() bool {
+	if u.index < len(u.users) {
+		return true
+	}
+	return false
+
+}
+
+func (u *userIterator) getNext() *user {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
